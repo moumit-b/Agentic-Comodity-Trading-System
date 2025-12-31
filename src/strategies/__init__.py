62ad316e1f7bd563ab7023d49e0a7538b5b01346
moumit-b <moumitb@bu.edu>
2025-12-31 02:20:46 -0500

    Phase 2 with implementation of stratey framework; tests passed
    Signed-off-by: moumit-b <moumitb@bu.edu>

--- src/strategies/__init__.py
+++ src/strategies/__init__.py
@@ -1,11 +1,21 @@
 """Trading strategy modules."""
 
-from .base import BaseStrategy, IndicatorSet, Signal, StrategyType, TradingHorizon
+from .base import BaseStrategy, IndicatorSet, Signal, SignalDirection, StrategyType, TradingHorizon
+from .mean_reversion import BollingerBandsMeanReversion, RSIOversoldOverbought
+from .trend_following import EMACrossoverStrategy, MACDTrendStrategy
 
 __all__ = [
+    # Base
     "BaseStrategy",
     "IndicatorSet",
     "Signal",
+    "SignalDirection",
     "StrategyType",
     "TradingHorizon",
+    # Trend Following
+    "EMACrossoverStrategy",
+    "MACDTrendStrategy",
+    # Mean Reversion
+    "BollingerBandsMeanReversion",
+    "RSIOversoldOverbought",
 ]
